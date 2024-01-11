5cd53010fd23912d200d0f72d063abb81ca6251f
shriyans jindal <shriyansjindal40517@gmail.com>
2024-01-11 16:10:30 +0530

    done

---
+++ nutrition/src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './App.css'
+import FoodBox from './Component/FoodBox'
+import FoodData from './resources/FoodData'
+export default class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      value:"",
+      newarr:FoodData,
+    }
+    
+  }
+  render() {
+    let {newarr}= this.state
+    let handlechange=(e)=>{
+      
+    // console.log(e.target.value)
+    let filter = FoodData.filter((el)=>el.name.includes(e.target.value))
+    this.setState({newarr:filter})
+    }    
+    return (
+      <>
+      <div>
+        <input type="text" placeholder='search' onChange={handlechange}  />
+      </div>
+      {newarr.map((el,i)=><FoodBox key={i} {...el}/>)}
+      </>
+    )
+  }
+}
